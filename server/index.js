a0449b89792b6bf18cd8bbca4ec15d24aabfe209
syu-y <syu12580526@gmail.com>
2020-10-19 02:03:08 +0900

    endpoint作成

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/products', (req, res) => {
+  res.json({ 'success': true });
+});
+
 const PORT = process.env.PORT || '3001';
 
 app.listen(PORT, () => {
