656deb66d540365a39c38ae82b42a2e72818fae6
syu-y <syu12580526@gmail.com>
2020-10-18 11:03:21 +0900

    商品詳細情報の追加実装

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,21 +2,49 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    heading1: 'Sample Header1',
+    heading2: 'Sample Header2',
+    heading3: 'Sample Header3',
+    headingtext1: 'Sample text1',
+    headingtext2: 'Sample text2',
+    headingtext3:
+      'Sample text3. Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
   },
   {
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    heading1: 'Sample Header1',
+    heading2: 'Sample Header2',
+    heading3: 'Sample Header3',
+    headingtext1: 'Sample text1',
+    headingtext2: 'Sample text2',
+    headingtext3:
+      'Sample text3. Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
   },
   {
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: '',
+    heading1: 'Sample Header1',
+    heading2: 'Sample Header2',
+    heading3: 'Sample Header3',
+    headingtext1: 'Sample text1',
+    headingtext2: 'Sample text2',
+    headingtext3:
+      'Sample text3. Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
   },
   {
     name: 'Phone 11',
     price: 1000,
-    description: ''
-  }
+    description: '',
+    heading1: 'Sample Header1',
+    heading2: 'Sample Header2',
+    heading3: 'Sample Header3',
+    headingtext1: 'Sample text1',
+    headingtext2: 'Sample text2',
+    headingtext3:
+      'Sample text3. Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
+  },
 ];
