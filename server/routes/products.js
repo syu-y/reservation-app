c9e0487a3c0fc5679d1aa79f1cbbaf74dc1c08a1
syu-y <syu12580526@gmail.com>
2020-10-21 00:21:27 +0900

    全件検索API追加

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const Product = require('../model/product');
 
 const router = express.Router();
 
 router.get('', (req, res) => {
-  res.json({ 'ok': true });
+  Product.find({}, (err, foundProducts) => {
+    res.json(foundProducts);
+  });
 });
 
 module.exports = router;
