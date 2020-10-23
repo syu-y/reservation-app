e8aa80168d8171020dc7e42f7d49e1924cbe900f
syu-y <syu12580526@gmail.com>
2020-10-24 03:45:16 +0900

    DBの初期化とパスの設定を環境ごとに設定

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 
-const config = require('./config/dev');
+const config = require('./config');
 const SampleDb = require('./sample-db');
 const productRouter = require('./routes/products');
 
@@ -11,19 +11,23 @@ mongoose.connect(config.DB_URI, {
   useUnifiedTopology: true
 }).then(
   () => {
-    const sampleDb = new SampleDb();
-    sampleDb.initDb();
+    if (process.env.NODE_ENV !== 'produciton') {
+      const sampleDb = new SampleDb();
+      // sampleDb.initDb();
+    }
   });
 
 const app = express();
 
 app.use('/api/v1/products', productRouter);
 
-const appPath = path.join(__dirname, '..', 'dist', 'reservation-app');
-app.use(express.static(appPath));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(appPath, 'index.html'));
-});
+if (process.env.NODE_ENV === 'produciton') {
+  const appPath = path.join(__dirname, '..', 'dist', 'reservation-app');
+  app.use(express.static(appPath));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(appPath, 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || '3001';
 
