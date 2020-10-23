07605e6dd0d05e6e1ec86b74c7d4378adacee0e2
syu-y <syu12580526@gmail.com>
2020-10-24 03:32:14 +0900

    serverのindex.jsにパスの設定を追記

--- server/index.js
+++ server/index.js
@@ -1,22 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+
 const config = require('./config/dev');
 const SampleDb = require('./sample-db');
-
 const productRouter = require('./routes/products');
 
-mongoose.connect(config.DB_URI,{
+mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(
   () => {
     const sampleDb = new SampleDb();
     sampleDb.initDb();
-});
+  });
 
 const app = express();
 
 app.use('/api/v1/products', productRouter);
+
+const appPath = path.join(__dirname, '..', 'dist', 'reservation-app');
+app.use(express.static(appPath));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(appPath, 'index.html'));
+});
 
 const PORT = process.env.PORT || '3001';
 
